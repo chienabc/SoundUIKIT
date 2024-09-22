64d4af72a097ac134ed3365f588df7299c58eff8
chienabc <chien97666@gmail.com>
2024-09-22 12:04:12 +0700

    phát âm thanh

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -6,24 +6,50 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    var audioPlayer: AVAudioPlayer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
     
     // LÀM ANIMATION
     // ÂM THANH
     // TIMER, AVAUDIOPLAYER
-    
-    
     // BUỎI 4 - ÔN TẬP
     // 1. GITHUB
     // setup môi trường, cấu hình
     // 100% DEV -> GITHUB
     // đọc doc apple -> AVAudioPlayer
  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func Start(_ sender: Any) {
+        
+        guard let sound = Bundle.main.path(forResource: "Music-G", ofType: "mp3")
+        else {
+            print("file not found")
+            return
+        }
+        
+        let url = URL(fileURLWithPath: sound)
+        
+            // xử lý ngoại lệ
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.play()
+        } catch {
+            print("Error file not found")
+        }
+//        audioPlayer.pause()
+    }
+    
+    
+    
+    
 }
 
